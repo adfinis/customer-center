f07ac86b6272ce5190e06fb269c4be02588d0399
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-06-30 09:25:55 +0200

    Base to build upon, based on endpoints example

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
-import express from 'express'
-import morgan  from 'morgan'
+import fs         from 'fs'
+import path       from 'path'
+import express    from 'express'
+import bodyParser from 'body-parser'
+import morgan     from 'morgan'
+import API        from './classes/api'
 
-let app = express()
+const app = express()
 export default app
 
-let env = app.get('env')
+const env = app.get('env')
 
 /* istanbul ignore next */
 if (env === 'production') {
@@ -14,7 +18,23 @@ else if (env === 'development') {
   app.use(morgan('dev'))
 }
 
-app.get('/api', (req, res) => {
-  res.type('text')
-  res.send(`Hello World ${process.env.TEST}!`)
+app.use(bodyParser.json({
+  type: [ 'application/json', 'application/vnd.api+json' ]
+}))
+
+const modulePath = path.join(__dirname, 'modules')
+const resources  = fs.readdirSync(modulePath)
+
+for (let resource of resources) {
+  API.register(resource)
+  app.use(API.enpoint(resource))
+}
+
+app.get('/v1', (req, res) => {
+  res.set('Content-Type', 'application/json')
+  res.send(JSON.stringify(API.index(), null, 2))
+})
+
+app.get('/', (req, res) => {
+  res.redirect('/api/v1')
 })
---
+++ backend/src/classes/api.js
@@ -0,0 +1,19 @@
+import path         from 'path'
+import express      from 'express'
+import routeBuilder from 'express-routebuilder'
+import Endpoints    from 'endpoints'
+
+export default new Endpoints.Application({
+  searchPaths: [ path.join(__dirname, '..', 'modules') ],
+
+  routeBuilder(routes, prefix) {
+    return routeBuilder(new express.Router(), routes, prefix)
+  },
+
+  Controller: Endpoints.Controller.extend({
+    baseUrl:    '/v1',
+    store:      Endpoints.Store.bookshelf,
+    format:     Endpoints.Format.jsonapi,
+    validators: [ Endpoints.ValidateJsonSchema ]
+  })
+})

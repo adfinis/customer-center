b430c68871c39e0f32148aed3404399ae4a35e09
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-07-01 07:31:37 +0200

    Disable form while signing in.

---
+++ frontend/app/login/controller.js
@@ -0,0 +1,13 @@
+import Ember from 'ember'
+import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin'
+
+export default Ember.Controller.extend(LoginControllerMixin, {
+  authenticator: 'authenticator:custom',
+  actions: {
+    authenticate() {
+      this.set('loading', true)
+
+      this._super().finally(() => this.set('loading', false))
+    }
+  }
+})

f08213f37a868e9509a39b86b565551daa4804bc
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-07-03 11:12:17 +0200

    Reset password-reset controller identification

--- frontend/app/login/password-reset/route.js
+++ frontend/app/login/password-reset/route.js
@@ -1,6 +1,10 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
+  resetController(controller) {
+    controller.set('identification', null)
+  },
+
   renderTemplate() {
     this.render({
       into: 'application'

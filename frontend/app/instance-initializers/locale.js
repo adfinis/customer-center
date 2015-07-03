fffc632d324ba8b0ab61e0b78810dd05d354edbd
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-07-03 10:43:43 +0200

    Try to set locale depending on browser language

---
+++ frontend/app/instance-initializers/locale.js
@@ -0,0 +1,32 @@
+export function initialize(instance) {
+  let locale = getNavigatorLanguage()
+
+  if (locale.includes('-')) {
+    locale = locale.split('-')[0]
+  }
+
+  if (locale.includes('_')) {
+    locale = locale.split('_')[0]
+  }
+
+  if (locale) {
+    let i18n = instance.container.lookup('service:i18n')
+
+    if (i18n.get('locales').includes(locale)) {
+      instance.container.lookup('service:i18n').set('locale', locale)
+    }
+  }
+}
+
+function getNavigatorLanguage() {
+  let navigator = window.navigator
+  let locale    = navigator.languages ? navigator.languages[0] : null
+
+  return locale || navigator.language || navigator.browserLanguage || navigator.userLanguage
+}
+
+export default {
+  name: 'locale',
+  after: 'ember-i18n',
+  initialize
+}

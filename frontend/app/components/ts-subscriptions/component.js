bc6f4f52a71e990ca352a3c97c77a2e54dec62f4
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2015-07-10 10:20:10 +0200

    naming convention api -> apiKey - changend in timescout backend

--- frontend/app/components/ts-subscriptions/component.js
+++ frontend/app/components/ts-subscriptions/component.js
@@ -3,8 +3,9 @@ import Ember from 'ember';
 const { $ } = Ember
 
 export default Ember.Component.extend({
+  apiAction: 'projects',
   fetchData: Ember.on('init', function() {
-    let params = {user: 'Fromarte', action: 'projects'}
+    let params = { user: this.get('user'), action: this.get('apiAction') }
 
     $.getJSON('/api/proxy/timescout/service/api.php', params)
       .then(res => this.set('model', res))

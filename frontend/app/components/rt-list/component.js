3af92e6bb75ff594ca81e827fb5db74afe0fa305
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-07-20 17:56:50 +0200

    Uppercase first letter of rt status

--- frontend/app/components/rt-list/component.js
+++ frontend/app/components/rt-list/component.js
@@ -5,6 +5,13 @@ export default Ember.Component.extend({
     this._super(...args)
 
     Ember.$.getJSON('/api/rt/tickets')
+      .then(res => {
+        res.data.tickets.forEach(t =>
+          t.status = `${t.status[0].toUpperCase()}${t.status.slice(1)}`
+        )
+
+        return res
+      })
       .then(res => this.set('model', res))
   }
 })

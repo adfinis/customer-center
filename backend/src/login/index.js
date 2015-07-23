bcf3347ad27a461607fe93cf07d30674e375e3cd
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-07-23 08:39:51 +0200

    Add services to session user

--- backend/src/login/index.js
+++ backend/src/login/index.js
@@ -21,6 +21,7 @@ passport.serializeUser((id, done) => {
   user.username  = id.uid
   user.shortname = id.sn
   user.groups    = id._groups.map(g => g.cn)
+  user.services  = getUserServices(user)
 
   console.log('Serialized user', user)
 
@@ -30,6 +31,29 @@ passport.deserializeUser((id, done) => {
   console.log('Deserialized user', id)
   done(null, id)
 })
+
+function getUserServices(user) {
+  let services = []
+
+  for (let service of config.services) {
+    if (!service.ldapGroup) {
+      services.push({ type: service.type })
+      continue
+    }
+
+    let userGroups = user.groups.filter(g =>
+      g.endsWith(service.type)
+    )
+
+    for (let userGroup of userGroups) {
+      let sn = userGroup.split('-')[1]
+
+      services.push({ type: service.type, sn })
+    }
+  }
+
+  return services
+}
 
 const router = new express.Router
 export default router
@@ -43,10 +67,11 @@ router.post('/login', (req, res, next) =>
       /* istanbul ignore if */
       if (loginError) return next(loginError)
 
+      let { user } = req._passport.session
       let claims = {
         iss:    config.application.name,
         aud:    config.application.host,
-        user:   { groups: req._passport.session.user.groups }
+        user:   { groups: user.groups }
       }
 
       req.session.create(claims, (sessionError, token) => {
@@ -54,7 +79,7 @@ router.post('/login', (req, res, next) =>
         if (sessionError) return next(sessionError)
 
         res.set('Content-Type', 'application/vnd.api+json')
-        res.send({ data: { token } })
+        res.send({ data: { token, user } })
       })
     })
   })(req, res, next)

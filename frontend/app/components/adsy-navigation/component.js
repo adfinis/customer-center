1c4f6871628488fd013d3282e05a113eb06a6f46
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-07-30 16:12:25 +0200

    FF navigation fix and liquid-fire scrolling fix

--- frontend/app/components/adsy-navigation/component.js
+++ frontend/app/components/adsy-navigation/component.js
@@ -3,6 +3,12 @@ import Ember from 'ember'
 export default Ember.Component.extend({
   classNames: [ 'flex-grow' ],
 
+  didInsertElement() {
+    this.$('nav').one('animationend animationend webkitAnimationEnd oanimationend MSAnimationEnd', () => {
+      this.$('nav').removeClass('slide-in')
+    })
+  },
+
   actions: {
     toggle() {
       this.$('.nav-side').toggleClass('nav-side--expand')

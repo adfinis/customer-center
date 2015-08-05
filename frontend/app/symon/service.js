d1fa9097cf18656f75023eb1623fc2dfa354d12a
Adrian Wittwer <adrian.wittwer@adfinis-sygroup.ch>
2015-08-05 17:17:19 +0200

    created symon service

---
+++ frontend/app/symon/service.js
@@ -0,0 +1,27 @@
+import Ember from 'ember'
+import ajax  from 'ic-ajax'
+
+export default Ember.Service.extend({
+
+  /**
+   * Symon service for fetching hosts and services
+   *
+   * @class SymonService
+   * @private
+   */
+  request(params) {
+    let data = Object.assign({}, params)
+
+    return ajax('/api/proxy/symon', { data })
+  },
+
+  /**
+   * Fetch symon hosts
+   *
+   * @return {Object}
+   * @public
+   */
+  fetchHosts() {
+    return this.request()
+  }
+});

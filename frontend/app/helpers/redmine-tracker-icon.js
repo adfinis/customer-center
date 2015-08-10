056ef0f3945402bc061428b4ef9743925fc2f3ce
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-08-10 07:53:01 +0200

    Move redmine tracker icon into a helper

---
+++ frontend/app/helpers/redmine-tracker-icon.js
@@ -0,0 +1,21 @@
+import Ember from 'ember'
+
+const { Handlebars, Helper } = Ember
+const { escapeExpression }   = Handlebars.Utils
+
+const ICONS = {
+  'Task':    'tasks',
+  'Bug':     'bug',
+  'Feature': 'lightbulb-o',
+  'Support': 'support',
+  'Project': 'folder-open-o'
+}
+
+export function redmineTrackerIcon([ trackerName ]) {
+  let icon = ICONS[trackerName] || 'puzzle-piece'
+  return new Handlebars.SafeString(
+    `<i class="fa fa-${icon} title="${escapeExpression(trackerName)}"></i>`
+  )
+}
+
+export default Helper.helper(redmineTrackerIcon)
---
+++ frontend/tests/unit/helpers/redmine-tracker-icon-test.js
@@ -0,0 +1,10 @@
+import { redmineTrackerIcon } from '../../../helpers/redmine-tracker-icon';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | redmine tracker icon');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = redmineTrackerIcon(42);
+  assert.ok(result);
+});

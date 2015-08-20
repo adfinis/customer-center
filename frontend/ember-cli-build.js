fa1f4fd4c14b6cb4baf3de17243291b922256505
Damian Senn <damian.senn@adfinis-sygroup.ch>
2015-08-20 12:42:56 +0200

    Use ember-adcssy instead of manually including

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -1,7 +1,6 @@
 /* global require, module */
 
 var EmberApp = require('ember-cli/lib/broccoli/ember-app')
-var funnel   = require('broccoli-funnel')
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
@@ -11,19 +10,8 @@ module.exports = function(defaults) {
     hinting: false
   })
 
-  var adcssyAssets = funnel('bower_components/adcssy/assets', {
-    srcDir: '/',
-    include: [ 'fonts/*', 'pictures/**/*' ],
-    destDir: '/'
-  })
-
-  var fontAwesome = funnel('node_modules/font-awesome/fonts', {
-    destDir: '/fonts'
-  })
-
   app.import('vendor/babel-polyfill.js', { prepend: true })
   app.import('bower_components/moment/locale/de.js')
-  app.import('bower_components/adcssy/build/css/adcssy.css')
 
-  return app.toTree([ adcssyAssets, fontAwesome ])
+  return app.toTree()
 }

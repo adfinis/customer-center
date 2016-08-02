089121e5e8c621707a1db48e408279af427c7eb0
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2016-08-02 08:55:37 +0200

    started talking to vault in docker

--- frontend/app/vault/index/route.js
+++ frontend/app/vault/index/route.js
@@ -1,18 +1,10 @@
 import Ember from 'ember'
 
 export default Ember.Route.extend({
+
+  vault: Ember.inject.service(),
+
   model() {
-    const dummyData = {
-      systems: [
-        'vm-customer-1.tld',
-        'vm-customer-1.tld'
-      ],
-      firewalls: [
-        'ieu04-fw01.sn-adm.iet-gibb.ch',
-        'ieu04-fw02.sn-adm.iet-gibb.ch',
-        'ieu04-fw03.sn-adm.iet-gibb.ch'
-      ]
-    }
-    return dummyData
+    return this.get('vault').request()
   }
 })
---
+++ frontend/app/vault/service.js
@@ -0,0 +1,25 @@
+import Ember  from 'ember'
+
+/**
+ * Vault service for fetching secrets
+ *
+ * @class VaultService
+ * @public
+ */
+export default Ember.Service.extend({
+
+  ajax: Ember.inject.service(),
+
+  /**
+   * Vault API requests
+   *
+   * @param {string} action The timescout API action
+   * @param {Object} params Request query params
+   * @param {string} type   Request type
+   * @return {Promise}
+   * @private
+   */
+  request() {
+    return this.get('ajax').request('/api/proxy/vault/v1/secret?list=true')
+  }
+})

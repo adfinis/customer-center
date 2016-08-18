365991220bc06d27f401fbbf4ad2f2951f1c2ae2
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2016-08-18 09:13:43 +0200

    Add notifications for vault actions

--- frontend/app/vault/edit/route.js
+++ frontend/app/vault/edit/route.js
@@ -6,6 +6,8 @@ export default Ember.Route.extend({
 
   vault: Ember.inject.service(),
 
+  notify: Ember.inject.service(),
+
   model({ path }) {
     return RSVP.hash({
       details: this.get('vault').details(path),
@@ -19,12 +21,14 @@ export default Ember.Route.extend({
     async save(secrets) {
       const path = this.modelFor(this.routeName).rawPath
       await this.get('vault').del(path)
-      this.get('vault').save(path, secrets)
+      await this.get('vault').save(path, secrets)
+      this.get('notify').success('Node was saved.')
     },
 
     async deleteNode() {
       const path = this.modelFor(this.routeName).rawPath
       await this.get('vault').del(path)
+      this.get('notify').success('Node was deleted.')
       this.transitionTo('vault')
     }
   }

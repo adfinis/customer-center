35ebab9b18d9ff64e1a3b61e1e19524158e0ff96
Christian Zosel <christian.zosel@adfinis-sygroup.ch>
2017-09-14 13:56:56 +0200

    Fix logout button

--- frontend/app/components/cc-sidenav-body/component.js
+++ frontend/app/components/cc-sidenav-body/component.js
@@ -1,3 +1,9 @@
 import Ember from 'ember'
 
-export default Ember.Component.extend({})
+export default Ember.Component.extend({
+  actions: {
+    logout() {
+      this.get('on-logout')()
+    }
+  }
+})

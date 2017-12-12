9344fe5ab52320fa98dd592351403114d6aa717f
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-12-12 14:58:31 +0100

    Cleanup work
    * Move backend eslint / prettier config to backend folder
    * Enforce prettier
    * Remove useless ember-sy dependency
    * Remove useless test file

---
+++ backend/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  parser: 'babel-eslint',
+  env: {
+    node: true,
+    es6: true
+  },
+  plugins: ['prettier'],
+  extends: ['eslint:recommended', 'prettier'],
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': 'error'
+  }
+}

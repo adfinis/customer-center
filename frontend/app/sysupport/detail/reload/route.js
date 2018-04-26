49600a3f96ad7995291216b7b90163fdb13a5b5a
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-04-26 17:12:26 +0200

    move logic from detail/reload route to controller

--- frontend/app/sysupport/detail/reload/route.js
+++ frontend/app/sysupport/detail/reload/route.js
@@ -1,49 +1,21 @@
 import Route from '@ember/routing/route'
 import { inject as service } from '@ember/service'
-import { translationMacro as t } from 'ember-i18n'
+import RSVP from 'rsvp'
 
 export default Route.extend({
-  notify: service(),
   i18n: service(),
 
-  model() {
-    return this.store.findAll('timed-subscription-package')
-  },
-
-  setupController(controller, model) {
-    controller.setProperties({
-      model,
-      breadCrumb: {
-        title: t('sysupport.breadcrumbs.reload')
-      }
+  init() {
+    this._super(...arguments)
+    this.set('breadCrumb', {
+      title: this.get('i18n').t('sysupport.breadcrumbs.reload')
     })
   },
 
-  actions: {
-    subscribe(subscriptionPackage) {
-      this._subscribe(subscriptionPackage)
-    }
-  },
-
-  _subscribe(subscriptionPackage) {
-    let order = {
-      duration: subscriptionPackage.get('duration'),
-      project: this.modelFor('sysupport/detail')
-    }
-    this.store
-      .createRecord('timed-subscription-order', order)
-      .save()
-      .then(() => {
-        this.get('notify').success(
-          this.get('i18n').t('sysupport.reload.success')
-        )
-        this.transitionTo(
-          'sysupport.detail.index',
-          this.modelFor('sysupport/detail').id
-        )
-      })
-      .catch(() => {
-        this.get('notify').error(this.get('i18n').t('sysupport.reload.error'))
-      })
+  model() {
+    return RSVP.hash({
+      project: this.modelFor('sysupport/detail'),
+      packages: this.store.findAll('timed-subscription-package')
+    })
   }
 })

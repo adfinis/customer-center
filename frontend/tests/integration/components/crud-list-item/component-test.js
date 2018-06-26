ce7fa841a0071908526331083f17b48cd34e05d6
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-06-26 08:29:57 +0200

    import click in integration test

--- frontend/tests/integration/components/crud-list-item/component-test.js
+++ frontend/tests/integration/components/crud-list-item/component-test.js
@@ -1,7 +1,7 @@
 import EmberObject from '@ember/object'
 import { module, test } from 'qunit'
 import { setupRenderingTest } from 'ember-qunit'
-import { render, settled } from '@ember/test-helpers'
+import { render, settled, click } from '@ember/test-helpers'
 import hbs from 'htmlbars-inline-precompile'
 
 module('Integration | Component | crud list item', function(hooks) {

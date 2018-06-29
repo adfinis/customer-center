856eabb718303425e4ee7aca74a44b51603dd51d
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-06-29 19:53:27 +0200

    remove "-" from minutes if duration is negative

--- frontend/app/helpers/format-duration-short.js
+++ frontend/app/helpers/format-duration-short.js
@@ -1,12 +1,17 @@
 import Helper from '@ember/component/helper'
 import startPaddingTag from 'customer-center/utils/start-padding-tag'
+import moment from 'moment'
 
 const { trunc } = Math
 
 export function formatDurationShort([duration]) {
-  return startPaddingTag(2)`${trunc(
-    duration.as('hours')
+  //remove "-" from negative numbers
+  let negative = duration < 0
+  duration = negative ? moment.duration(-duration) : duration
+  let str = startPaddingTag(2)`${trunc(
+    duration.asHours()
   )}:${duration.minutes()}`
+  return negative ? `-${str}` : str
 }
 
 export default Helper.helper(formatDurationShort)

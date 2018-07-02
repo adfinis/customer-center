62e7872808553d089d03c41664a18dd99607a96e
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-07-02 09:53:52 +0200

    fix format-duration integration test

--- frontend/tests/integration/helpers/format-duration-test.js
+++ frontend/tests/integration/helpers/format-duration-test.js
@@ -17,7 +17,7 @@ module('Integration | Helper | format duration', function(hooks) {
     this.set('duration', moment.duration({ hours: 2, minutes: 5 }))
     assert.dom('*').hasText('2 Hours 5 Minutes')
 
-    this.set('duration', moment.duration({ hours: -2, minutes: 5 }))
+    this.set('duration', moment.duration({ hours: -2, minutes: -5 }))
     assert.dom('*').hasText('-2 Hours 5 Minutes')
   })
 })

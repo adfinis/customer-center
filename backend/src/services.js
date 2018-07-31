b887b76708549f0c26473e5eea0a03a7146030c0
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-07-31 13:59:47 +0200

    add rt proxy and docker config. we only build the rt container local until we have ci/cd configured to add rt to our registry.

--- backend/src/services.js
+++ backend/src/services.js
@@ -3,11 +3,12 @@ import VaultProxy from './vault/vault-proxy'
 import vaultCustom from './vault/vault-custom'
 import TimedProxy from './timed/proxy'
 import gitlabProxy from './gitlab/proxy'
+import rtProxy from './rt/proxy'
 import config from './config'
 
 let router = new express.Router()
 
-const { vault, timed, gitlab } = config.services
+const { vault, timed, gitlab, rt } = config.services
 
 router.use('/vault', vaultCustom(vault))
 router.use('/proxy/vault', VaultProxy.createProxy(vault))
@@ -15,5 +16,7 @@ router.use('/proxy/vault', VaultProxy.createProxy(vault))
 router.use('/proxy/timed', TimedProxy.createProxy(timed))
 
 router.use('/proxy/gitlab', gitlabProxy(gitlab))
+
+router.use('/proxy/rt', rtProxy(rt))
 
 export default router

edb48c5872b9c3f7c1116fc175614a4fc4747e98
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-07-31 19:53:44 +0200

    add models and refactor adapter since we now have other ember data models besides timed

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -2,7 +2,6 @@ import DS from 'ember-data'
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin'
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-  namespace: '/api/proxy/timed',
   authorize(xhr) {
     if (this.get('session.data.authenticated.data.token')) {
       xhr.setRequestHeader(

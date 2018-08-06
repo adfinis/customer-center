8f41c9a9bd18e744cd909a8b3c47b9ef9b248e61
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-08-06 02:10:39 +0200

    fix formatting in test

--- frontend/tests/unit/dashboard/route-test.js
+++ frontend/tests/unit/dashboard/route-test.js
@@ -1,11 +1,11 @@
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { module, test } from 'qunit'
+import { setupTest } from 'ember-qunit'
 
 module('Unit | Route | dashboard', function(hooks) {
-  setupTest(hooks);
+  setupTest(hooks)
 
   test('it exists', function(assert) {
-    let route = this.owner.lookup('route:dashboard');
-    assert.ok(route);
-  });
-});
+    let route = this.owner.lookup('route:dashboard')
+    assert.ok(route)
+  })
+})

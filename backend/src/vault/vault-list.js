3c5cf62295e9f6a1a29a2fb35db678139a614552
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-08-22 17:52:34 +0200

    improve performance
    by using http2 and by hiding nodes by default

--- backend/src/vault/vault-list.js
+++ backend/src/vault/vault-list.js
@@ -1,9 +1,7 @@
-import rp from 'request-promise'
+import http2 from 'http2'
 import wrap from 'express-async-wrap'
 
-import { getAuthenticator } from './vault-custom'
-
-let host, prefix, auth
+let host, prefix
 
 function stripPrefix(path) {
   if (path.startsWith(prefix)) {
@@ -12,30 +10,52 @@ function stripPrefix(path) {
   return path
 }
 
-async function listVault(token, path) {
-  const rawResponse = await rp(
-    auth(token, {
-      forever: true,
-      uri: `${host}${path}?list=true`
-    })
-  )
-  const resp = JSON.parse(rawResponse)
+async function listVault(token, path, client) {
   const result = {}
-  if (resp.data && resp.data.keys) {
-    result.values = resp.data.keys
-      .filter(key => !key.endsWith('/'))
-      .reduce((res, key) => {
-        res[key] = { path: stripPrefix(path + key) }
-        return res
-      }, {})
 
-    result.children = {}
-    await Promise.all(
-      resp.data.keys.filter(key => key.endsWith('/')).map(async key => {
-        result.children[key] = await listVault(token, path + key)
-      })
-    )
-  }
+  const req = client.request({
+    ':path': encodeURI(path) + '?list=true',
+    'X-Vault-Token': token
+  })
+
+  req.setEncoding('utf8')
+
+  // IMPORTANT: the data event can also be called if the sever has not yet
+  // returned the whole data set. We need to concatenate untill the request ends.
+  await new Promise(resolve => {
+    let data = ''
+    req.on('data', async rawResponse => {
+      data += rawResponse
+    })
+    req.on('end', async () => {
+      try {
+        const resp = JSON.parse(data)
+        if (resp.data && resp.data.keys) {
+          result.values = resp.data.keys
+            .filter(key => !key.endsWith('/'))
+            .reduce((res, key) => {
+              res[key] = { path: stripPrefix(path + key) }
+              return res
+            }, {})
+
+          result.children = {}
+          await Promise.all(
+            resp.data.keys.filter(key => key.endsWith('/')).map(async key => {
+              result.children[key] = await listVault(token, path + key, client)
+            })
+          )
+        } else {
+          result.values = {}
+          result.children = {}
+        }
+      } catch (e) {
+        console.log(path, ':', e)
+      } finally {
+        data = ''
+      }
+      resolve()
+    })
+  })
 
   return result
 }
@@ -43,16 +63,18 @@ async function listVault(token, path) {
 export default function vaultListhandler(service) {
   host = service.host
   prefix = service.prefix
-  auth = getAuthenticator(service.ca)
 
   return wrap(async (req, res) => {
     try {
-      res.send(
-        await listVault(
-          req.session.vaultToken,
-          service.prefix + service.backend
-        )
+      const client = http2.connect(host)
+
+      let response = await listVault(
+        req.session.vaultToken,
+        service.prefix + service.backend,
+        client
       )
+      client.destroy()
+      res.send(response)
     } catch (e) {
       res.status(e.statusCode).send(e.message)
     }

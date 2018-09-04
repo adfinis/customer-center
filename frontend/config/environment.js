b74919392cdbe6fe74b01e36bcdd9ce0715fa15e
Jonas Cosandey <jonas.cosandey@adfinis-sygroup.ch>
2018-09-04 15:07:28 +0200

    add gitlab and rt to prod services

--- frontend/config/environment.js
+++ frontend/config/environment.js
@@ -65,7 +65,7 @@ module.exports = function(environment) {
 
   if (environment === 'production') {
     // Services : redmine, mon, timed, wiki, rt, vault
-    ENV.APP.enabledServices = ['vault', 'timed']
+    ENV.APP.enabledServices = ['vault', 'timed', 'gitlab', 'rt']
   }
 
   return ENV

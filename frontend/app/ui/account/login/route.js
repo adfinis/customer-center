83e411a782544b9a1c6d15c579437c532ddefec2
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2020-10-27 11:24:52 +0100

    fix: remove test credentials from login

--- frontend/app/ui/account/login/route.js
+++ frontend/app/ui/account/login/route.js
@@ -3,8 +3,8 @@ import BaseRoute from "customer-center/routes/-base";
 export default class AccountLoginRoute extends BaseRoute {
   model() {
     return {
-      username: "admin1",
-      password: "123qweasd",
+      username: "",
+      password: "",
     };
   }
 }

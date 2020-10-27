d1ab111d5404e60f5ee988b9c6ae8e33ac0e96ad
Florian Mäder <florian.maeder@adfinis-sygroup.ch>
2020-10-27 16:19:21 +0100

    fix(frontend): remove obsolete code
    The language change is now handled in the account service.

--- frontend/app/ui/application/controller.js
+++ frontend/app/ui/application/controller.js
@@ -1,12 +1,10 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { inject as service } from "@ember/service";
-import moment from "moment";
 
 export default class ApplicationController extends Controller {
   @service account;
   @service session;
-  @service moment;
   @service intl;
 
   get languages() {
@@ -17,9 +15,6 @@ export default class ApplicationController extends Controller {
   }
 
   @action async changeLanguage(language) {
-    this.intl.setLocale(language);
-    this.moment.setLocale(language);
-
     await this.account.changeLanguage(language);
   }
 }

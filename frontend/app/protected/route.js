ef41f2fde3dc4b197ac9aa6776cd6089361b2150
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-12-08 14:18:34 +0100

    fix: use inheritance correctly and allow mixins (warn)

--- frontend/app/protected/route.js
+++ frontend/app/protected/route.js
@@ -1,6 +1,13 @@
 import Route from "@ember/routing/route";
+import { inject as service } from "@ember/service";
 import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
 export default class ProtectedRoute extends Route.extend(
   AuthenticatedRouteMixin
-) {}
+) {
+  @service session;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(transition, "login");
+  }
+}

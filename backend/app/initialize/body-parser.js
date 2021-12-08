27febd3854cec4b94a12e54d8c7b9430da8fb375
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-12-08 15:11:51 +0100

    fix: delete backend directory
    This is not needed anymore as timed API will be used instead.

--- backend/app/initialize/body-parser.js
+++
@@ -1,9 +0,0 @@
-import bodyParser from 'body-parser';
-
-export default function initializeBodyParser(app) {
-  app.use(
-    bodyParser.json({
-      type: ['application/json', 'application/vnd.api+json'],
-    })
-  );
-}
--- backend/tests/helper.js
+++
@@ -1,9 +0,0 @@
-import chai, { expect } from 'chai';
-import chaiAsPromised from 'chai-as-promised';
-import chaiHttp from 'chai-http';
-
-chai.use(chaiAsPromised);
-chai.use(chaiHttp);
-
-global.request = chai.request;
-global.expect = expect;

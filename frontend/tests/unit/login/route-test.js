0ca967ddbed86b25dc32905996649ebfff891386
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-12-09 19:07:49 +0100

    fix: update tests

--- frontend/tests/unit/login/route-test.js
+++ frontend/tests/unit/login/route-test.js
@@ -2,12 +2,12 @@ import { setupIntl } from "ember-intl/test-support";
 import { setupTest } from "ember-qunit";
 import { module, test } from "qunit";
 
-module("Unit | Route | account/login", function (hooks) {
+module("Unit | Route | login", function (hooks) {
   setupTest(hooks);
   setupIntl(hooks, "en");
 
   test("it exists", function (assert) {
-    const route = this.owner.lookup("route:account/login");
+    const route = this.owner.lookup("route:login");
     assert.ok(route);
   });
 });

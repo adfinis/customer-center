4857901106e6246e752e2726bc20da355f214bd5
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-12-10 16:46:43 +0100

    feat(CI): allow configurable timed host

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,8 +1,12 @@
 import JSONAPIAdapter from "@ember-data/adapter/json-api";
+import ENV from "customer-center/config/environment";
 import OIDCAdapterMixin from "ember-simple-auth-oidc/mixins/oidc-adapter-mixin";
 
 const BaseAdapter = JSONAPIAdapter.extend(OIDCAdapterMixin);
 
 export default class ApplicationAdapter extends BaseAdapter {
   namespace = "api/v1";
+  host = window.location.host.includes("test")
+    ? ENV.APP.hostStaging
+    : ENV.APP.hostProd;
 }

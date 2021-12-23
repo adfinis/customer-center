71c8887fb887eff8051b9a5eedfd52223897cdc0
Akanksh Saxena <akanksh.saxena@adfinis-sygroup.ch>
2021-12-23 11:32:07 +0100

    chore: upgrade ember to v3.24

--- frontend/tests/test-helper.js
+++ frontend/tests/test-helper.js
@@ -1,8 +1,11 @@
-import { setApplication } from "@ember/test-helpers";
-import Application from "customer-center/app";
-import config from "customer-center/config/environment";
-import { start } from "ember-qunit";
+import { setApplication } from '@ember/test-helpers';
+import Application from 'customer-center/app';
+import config from 'customer-center/config/environment';
+import { start } from 'ember-qunit';
+import * as QUnit from 'qunit';
+import { setup } from 'qunit-dom';
 
 setApplication(Application.create(config.APP));
 
+setup(QUnit.assert);
 start();

179426966a197d6f2126c01d5767b25b74542d4f
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-07-03 14:41:00 +0200

    chore(deps): update eslint and node

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -4,8 +4,13 @@ module.exports = {
   settings: {
     "import/internal-regex": "^customer-center/",
   },
-  extends: ["@adfinis/eslint-config/ember-addon"],
+  extends: ["@adfinis/eslint-config/ember-app"],
   rules: {
     "ember/no-settled-after-test-helper": "warn",
+    "no-unused-vars": "off",
+    "prefer-rest-params": "off",
+    "ember/use-brace-expansion": "off",
+    "ember/no-get": "off",
+    "ember/no-mixins": "warn",
   },
 };
--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -1,11 +1,5 @@
 "use strict";
 
 module.exports = {
-  extends: "octane",
-
-  rules: {
-    "no-bare-strings": true,
-    "block-indentation": true,
-    "require-input-label": false,
-  },
+  extends: "recommended",
 };

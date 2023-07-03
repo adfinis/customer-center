24ac10f796c1008f9dc98ec463e50bb93043134c
Arthur Deierlein <arthur.deierlein@adfinis.com>
2023-07-03 16:36:42 +0200

    chore(lint): make template-lint errors into warnings

--- frontend/.template-lintrc.js
+++ frontend/.template-lintrc.js
@@ -2,4 +2,10 @@
 
 module.exports = {
   extends: "recommended",
+
+  rules: {
+    "no-invalid-link-text": "warn",
+    "require-input-label": "warn",
+    "no-autofocus-attribute": "warn",
+  },
 };
